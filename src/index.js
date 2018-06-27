f8b664d8f4e4403cae10be4031d32285d56e51b0
Phan Trung Sinh <phanlancerduk@hotmail.com>
2018-06-28 01:36:21 +0700

    add 404

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter } from 'react-router-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
+import configureStore from './services/store';
+
+import App from './App';
+import Page404 from './scenes/404';
+
+const history = createHistory();
+
+export const { store, persistor } = configureStore(undefined, history);
+
+const MOUNT_NODE = document.getElementById('root');
+
+render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor} loading={<div>Loading</div>}>
+        <ConnectedRouter history={history}>
+          <Switch>
+            <Route path="/" component={App} />
+            <Route component={Page404} />
+          </Switch>
+        </ConnectedRouter>
+    </PersistGate>
+  </Provider>,
+  MOUNT_NODE
+);
+
 registerServiceWorker();
