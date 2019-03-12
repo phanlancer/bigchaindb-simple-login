c032f63ba6071cc53a8fb761349dc612e5d54e0f
Phan Trung Sinh <phanlancerduk@hotmail.com>
2019-03-12 16:06:04 +0700

    app configuration definition

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,10 +1,11 @@
-const locale = 'en'; // default local is en
+const locale = "en"; // default local is en
 
 const APPCONFIG = {
   locale: locale,
-  apiURL: process.env.NODE_ENV === 'development'
-        ? 'https://test.bigchaindb.com/api/v1/'
-        : 'https://test.bigchaindb.com/api/v1/'   // set test net for demo version
-}
+  apiURL:
+    process.env.NODE_ENV === "development"
+      ? "https://test.bigchaindb.com/api/v1/"
+      : "https://test.bigchaindb.com/api/v1/" // set test net for demo version
+};
 
 export default APPCONFIG;
