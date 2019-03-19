d15304c34c952330cf3f663f24e161861b8479ff
Phan Trung Sinh <phanlancerduk@hotmail.com>
2019-03-19 20:09:18 +0700

    update circular progress comp

--- src/components/CircularProgress/index.js
+++ src/components/CircularProgress/index.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class CircularProgress extends Component {
   render() {
     return (
       <div className="fa-3x text-center">
-        <i className="fas fa-circle-notch fa-spin" style={{ color: 'var(--theme-color2)'}}></i>
+        <i
+          className="fas fa-circle-notch fa-spin"
+          style={{ color: "var(--theme-color2)" }}
+        />
       </div>
     );
   }
