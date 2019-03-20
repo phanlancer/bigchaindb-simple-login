aa1ec46bd97881c9c1ee351096b20f366ba30af1
Phan Trung Sinh <phanlancerduk@hotmail.com>
2019-03-20 07:39:14 +0700

    404 page update

--- src/scenes/404/components/404.js
+++ src/scenes/404/components/404.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Page404 = () => (
-  <section className="about section--gradient" style={{height: '70vh'}}>
+  <section className="about section--gradient" style={{ height: "70vh" }}>
     <div className="container">
       <div className="row">
         <div className="col-12">
-          <h2 className="section__title section__title--white section__title--margin">404 Page Not Found</h2>
+          <h2 className="section__title section__title--white section__title--margin">
+            404 Page Not Found
+          </h2>
         </div>
       </div>
     </div>
   </section>
-)
+);
 
 export default Page404;
