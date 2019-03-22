1c3968b247b848ebf0bb4de3aae3d0cba2438d80
Phan Trung Sinh <phanlancerduk@hotmail.com>
2019-03-22 21:57:40 +0700

    auth redux reform

--- src/services/actions/auth.js
+++ src/services/actions/auth.js
@@ -1,21 +1,21 @@
-import * as types from '../../constants/ActionTypes';
+import * as types from "../../constants/ActionTypes";
 
-export function loginAction (credential) {
+export function loginAction(credential) {
   return { type: types.ACTION_LOGIN, payload: credential };
 }
 
-export function logoutAction (redirect) {
+export function logoutAction(redirect) {
   return { type: types.ACTION_LOGOUT, payload: redirect };
 }
 
-export function registerAction (payload) {
+export function registerAction(payload) {
   return { type: types.ACTION_REGISTER, payload };
 }
 
-export function updateProfileAction (payload) {
+export function updateProfileAction(payload) {
   return { type: types.ACTION_UPDATE_PROFILE, payload };
 }
 
-export function updateAuthAction (payload) {
+export function updateAuthAction(payload) {
   return { type: types.ACTION_UPDATE_AUTH, payload };
 }
