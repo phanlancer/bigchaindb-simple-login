7c060ee46a4dc774cefbffa47fa0dbb9d0d43474
Phan Trung Sinh <phanlancerduk@hotmail.com>
2019-03-27 08:49:22 +0700

    json load

--- src/services/bigchaindb-driver/utils/ccJsonLoad.js
+++ src/services/bigchaindb-driver/utils/ccJsonLoad.js
@@ -1,6 +1,6 @@
-import { Buffer } from 'buffer'
-import base58 from 'bs58'
-import cc from 'crypto-conditions'
+import { Buffer } from "buffer";
+import base58 from "bs58";
+import cc from "crypto-conditions";
 
 /**
  * @public
@@ -9,33 +9,38 @@ import cc from 'crypto-conditions'
  * @returns {cc.Condition} Ed25519 Condition (that will need to wrapped in an Output)
  */
 export default function ccJsonLoad(conditionJson) {
-    if ('hash' in conditionJson) {
-        const condition = new cc.Condition()
-        condition.type = conditionJson.type_id
-        condition.bitmask = conditionJson.bitmask
-        condition.hash = Buffer.from(base58.decode(conditionJson.hash))
-        condition.maxFulfillmentLength = parseInt(conditionJson.max_fulfillment_length, 10)
-        return condition
-    } else {
-        let fulfillment
+  if ("hash" in conditionJson) {
+    const condition = new cc.Condition();
+    condition.type = conditionJson.type_id;
+    condition.bitmask = conditionJson.bitmask;
+    condition.hash = Buffer.from(base58.decode(conditionJson.hash));
+    condition.maxFulfillmentLength = parseInt(
+      conditionJson.max_fulfillment_length,
+      10
+    );
+    return condition;
+  } else {
+    let fulfillment;
 
-        if (conditionJson.type === 'threshold-sha-256') {
-            fulfillment = new cc.ThresholdSha256()
-            fulfillment.threshold = conditionJson.threshold
-            conditionJson.subconditions.forEach((subconditionJson) => {
-                const subcondition = ccJsonLoad(subconditionJson)
-                if ('getConditionUri' in subcondition) {
-                    fulfillment.addSubfulfillment(subcondition)
-                } else if ('serializeUri' in subcondition) {
-                    fulfillment.addSubcondition(subcondition)
-                }
-            })
+    if (conditionJson.type === "threshold-sha-256") {
+      fulfillment = new cc.ThresholdSha256();
+      fulfillment.threshold = conditionJson.threshold;
+      conditionJson.subconditions.forEach(subconditionJson => {
+        const subcondition = ccJsonLoad(subconditionJson);
+        if ("getConditionUri" in subcondition) {
+          fulfillment.addSubfulfillment(subcondition);
+        } else if ("serializeUri" in subcondition) {
+          fulfillment.addSubcondition(subcondition);
         }
-
-        if (conditionJson.type === 'ed25519-sha-256') {
-            fulfillment = new cc.Ed25519Sha256()
-            fulfillment.publicKey = Buffer.from(base58.decode(conditionJson.public_key))
-        }
-        return fulfillment
+      });
     }
+
+    if (conditionJson.type === "ed25519-sha-256") {
+      fulfillment = new cc.Ed25519Sha256();
+      fulfillment.publicKey = Buffer.from(
+        base58.decode(conditionJson.public_key)
+      );
+    }
+    return fulfillment;
+  }
 }
