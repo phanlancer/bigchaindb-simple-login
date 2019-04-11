47ede16773e8f278fc26c73e7d7dba3a077c3c8d
Phan Trung Sinh <phanlancerduk@hotmail.com>
2019-04-11 16:11:57 +0700

    indexing bigchain db driver

--- src/services/bigchaindb-driver/index.js
+++ src/services/bigchaindb-driver/index.js
@@ -1,5 +1,5 @@
-export { default as Ed25519Keypair } from './Ed25519Keypair';
-export { default as Connection } from './connection';
-export { default as Transaction } from './transaction';
-export { default as ccJsonLoad } from './utils/ccJsonLoad';
-export { default as ccJsonify } from './utils/ccJsonify';
+export { default as Ed25519Keypair } from "./Ed25519Keypair";
+export { default as Connection } from "./connection";
+export { default as Transaction } from "./transaction";
+export { default as ccJsonLoad } from "./utils/ccJsonLoad";
+export { default as ccJsonify } from "./utils/ccJsonify";
