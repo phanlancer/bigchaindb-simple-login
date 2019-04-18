acd3264b8fd7d659c1c7f5c49004d73854b52374
Phan Trung Sinh <phanlancerduk@hotmail.com>
2019-04-18 16:32:34 +0700

    bigchaindb connection

--- src/services/BigchaindbConnection/index.js
+++ src/services/BigchaindbConnection/index.js
@@ -1,9 +1,9 @@
-import { Connection } from '../bigchaindb-driver';
-import APPCONFIG from '../../constants/config';
+import { Connection } from "../bigchaindb-driver";
+import APPCONFIG from "../../constants/config";
 
 const BigchaindbConnection = new Connection(APPCONFIG.apiURL, {
   app_id: process.env.REACT_APP_BIGCHAINDB_APP_ID,
-  app_key: process.env.REACT_APP_BIGCHAINDB_APP_KEY,
+  app_key: process.env.REACT_APP_BIGCHAINDB_APP_KEY
 });
 
 export default BigchaindbConnection;
