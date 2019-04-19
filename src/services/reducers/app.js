aaabbfb6a11246a81800e476d279896e703d59e0
Phan Trung Sinh <phanlancerduk@hotmail.com>
2019-04-19 16:40:17 +0700

    update reducer app

--- src/services/reducers/app.js
+++ src/services/reducers/app.js
@@ -1,24 +1,24 @@
-import { LOCATION_CHANGE } from 'react-router-redux';
-import { ACTION_UPDATE_APP } from '../../constants/ActionTypes';
+import { LOCATION_CHANGE } from "react-router-redux";
+import { ACTION_UPDATE_APP } from "../../constants/ActionTypes";
 
 const initialState = {
   loading: false,
   error: false,
-  errorMessage: ''
+  errorMessage: ""
 };
 
 const app = (state = initialState, action) => {
   switch (action.type) {
     case ACTION_UPDATE_APP:
       return {
-          ...state,
-          ...action.payload
+        ...state,
+        ...action.payload
       };
     case LOCATION_CHANGE:
       return initialState;
     default:
       return state;
   }
-}
+};
 
 export default app;
