30806b14a37c74c90d8a4d2a5c6ea6d1a6a298a6
Phan Trung Sinh <phanlancerduk@hotmail.com>
2019-04-21 19:50:54 +0700

    sagas auth

--- src/services/sagas/auth.js
+++ src/services/sagas/auth.js
@@ -1,95 +1,153 @@
-import { call, put, select } from 'redux-saga/effects';
-import { push } from 'react-router-redux';
+import { call, put, select } from "redux-saga/effects";
+import { push } from "react-router-redux";
 
-import { updateAppAction } from '../actions/app';
-import { updateAuthAction } from '../actions/auth';
-import { login, register, updateProfile } from '../apis/auth';
+import { updateAppAction } from "../actions/app";
+import { updateAuthAction } from "../actions/auth";
+import { login, register, updateProfile } from "../apis/auth";
 
-export function * fetchLogin (action) {
-  console.log('--login---', action);
+export function* fetchLogin(action) {
+  console.log("--login---", action);
   try {
-    yield put(updateAppAction({ loading: true, error: false, errorMessage: '' }));
+    yield put(
+      updateAppAction({ loading: true, error: false, errorMessage: "" })
+    );
     const redirect = action.payload.redirect;
     const payload = {
       password: action.payload.password
     };
     const res = yield call(login, payload);
-    console.log('-res-', res);
+    console.log("-res-", res);
     if (res.errors !== undefined) {
-      console.log('-fail-');
-      yield put(updateAppAction({ loading: false, error: true, errorMessage: res.errors }));
+      console.log("-fail-");
+      yield put(
+        updateAppAction({
+          loading: false,
+          error: true,
+          errorMessage: res.errors
+        })
+      );
     } else {
-      console.log('-success-')
-      yield put(updateAppAction({ loading: false, error: false, errorMessage: '' }));
+      console.log("-success-");
+      yield put(
+        updateAppAction({ loading: false, error: false, errorMessage: "" })
+      );
       yield put(updateAuthAction({ authenticated: true, ...res }));
-      if (redirect !== '') {
+      if (redirect !== "") {
         yield put(push(redirect));
       }
-      yield put(push('/'));
+      yield put(push("/"));
     }
   } catch (error) {
-    yield put(updateAppAction({ loading: false, error: true, errorMessage: 'Invalid password' }));
-    console.log('-login error-', error);
+    yield put(
+      updateAppAction({
+        loading: false,
+        error: true,
+        errorMessage: "Invalid password"
+      })
+    );
+    console.log("-login error-", error);
   }
 }
 
-export function * fetchRegister (action) {
+export function* fetchRegister(action) {
   try {
-    yield put(updateAppAction({ loading: true, error: false, errorMessage: '' }));
+    yield put(
+      updateAppAction({ loading: true, error: false, errorMessage: "" })
+    );
 
     const res = yield call(register, action.payload);
-    console.log('-res-', res);
+    console.log("-res-", res);
     if (res.message !== undefined) {
-      console.log('-fail-');
-      yield put(updateAppAction({ loading: false, error: true, errorMessage: res.message }));
+      console.log("-fail-");
+      yield put(
+        updateAppAction({
+          loading: false,
+          error: true,
+          errorMessage: res.message
+        })
+      );
     } else {
-      console.log('-success-');
-      yield put(updateAppAction({ loading: false, error: false, errorMessage: '' }));
+      console.log("-success-");
+      yield put(
+        updateAppAction({ loading: false, error: false, errorMessage: "" })
+      );
       yield put(updateAuthAction({ authenticated: true, ...res }));
-      yield put(push('/'));
+      yield put(push("/"));
     }
   } catch (error) {
-
-    yield put(updateAppAction({ loading: false, error: true, errorMessage: 'Something goes wrong' }));
-    console.log('-register error-', error);
+    yield put(
+      updateAppAction({
+        loading: false,
+        error: true,
+        errorMessage: "Something goes wrong"
+      })
+    );
+    console.log("-register error-", error);
   }
 }
 
-export function * fetchUpdateProfile (action) {
+export function* fetchUpdateProfile(action) {
   try {
-    yield put(updateAppAction({ loading: true, error: false, errorMessage: '' }));
+    yield put(
+      updateAppAction({ loading: true, error: false, errorMessage: "" })
+    );
 
     const res = yield call(updateProfile, action.payload);
-    console.log('-res-', res);
+    console.log("-res-", res);
     if (res.message !== undefined) {
-      console.log('-fail-');
-      yield put(updateAppAction({ loading: false, error: true, errorMessage: res.message }));
+      console.log("-fail-");
+      yield put(
+        updateAppAction({
+          loading: false,
+          error: true,
+          errorMessage: res.message
+        })
+      );
     } else {
-      console.log('-success-');
-      yield put(updateAppAction({ loading: false, error: false, errorMessage: '' }));
+      console.log("-success-");
+      yield put(
+        updateAppAction({ loading: false, error: false, errorMessage: "" })
+      );
       yield put(updateAuthAction({ authenticated: true, ...res }));
     }
   } catch (error) {
-
-    yield put(updateAppAction({ loading: false, error: true, errorMessage: 'Something goes wrong' }));
-    console.log('-register error-', error);
+    yield put(
+      updateAppAction({
+        loading: false,
+        error: true,
+        errorMessage: "Something goes wrong"
+      })
+    );
+    console.log("-register error-", error);
   }
 }
 
-export function * fetchLogout (action) {
+export function* fetchLogout(action) {
   try {
     const redirect = action.payload;
     if (redirect === true) {
       const location = yield select(state => state.route.location);
       const pathname = location.pathname;
 
-      const url = '/login?redirect=' + encodeURIComponent(pathname);
+      const url = "/login?redirect=" + encodeURIComponent(pathname);
 
-      yield put(updateAuthAction({ currentIdentity: undefined, authenticated: false, me: {} }));
+      yield put(
+        updateAuthAction({
+          currentIdentity: undefined,
+          authenticated: false,
+          me: {}
+        })
+      );
       yield put(push(url));
     } else {
-      yield put(updateAuthAction({ currentIdentity: undefined, authenticated: false, me: {} }));
-      yield put(push('/'));
+      yield put(
+        updateAuthAction({
+          currentIdentity: undefined,
+          authenticated: false,
+          me: {}
+        })
+      );
+      yield put(push("/"));
     }
   } catch (error) {}
 }
