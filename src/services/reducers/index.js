9614db64e7e0152da5242248b60263b7eae9e692
Phan Trung Sinh <phanlancerduk@hotmail.com>
2019-04-21 14:08:12 +0700

    update reducers

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -1,8 +1,8 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
+import { combineReducers } from "redux";
+import { routerReducer } from "react-router-redux";
 
-import auth from './auth';
-import app from './app';
+import auth from "./auth";
+import app from "./app";
 
 const reducers = combineReducers({
   route: routerReducer,
